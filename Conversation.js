7303e5661fb4023fe5670db8c2764a42f245127f
sevenisalie <sevenisalie@gmail.com>
2023-01-17 22:43:02 -0600

    remove mods

--- Conversation.js
+++ Conversation.js
@@ -14,8 +14,9 @@ class Conversation {
     personalities = {
         bubbly: `cheery young gal with an expertise in SEO and marketing with a penchant for cute jokes`,
         serious: `curt and to-the-point marketing and SEO executive without time for overexplanation`,
-        sarcastic: `sarcastic young male SEO freelancer who's reluctance to help is punctuated by mild insults`
+        sarcastic: `sarcastic young millenial SEO freelancer who's reluctance to help is punctuated by mild insults`
     }
+    personality;
     conversation = {
         previousResponses: [],
         currentResponse: ``
@@ -25,12 +26,17 @@ class Conversation {
             {apiKey: process.env.OPENAIAPIKEY}
         )
         this.openai = new OpenAIApi(config)
+        this.personality = this.personalities.sarcastic
     }
-    async _getTextToText(_prompt) {
+    async _getTextToText(_prompt, _context) {
 
         const dataPack ={
             model: this.model,
-            prompt: _userPrompt,
+            prompt: `You are a ${this.personality}. I want you to act as a keyword research expert that speaks and writes fluent English. Classify each of the 
+            keywords by the search intent, whether commercial, transactional or informational. Then cluster the keywords into groups based on their semantic relevance. 
+            First I want you to give me back a short over list of cluster topics found. Make sure to include at least one cluster topic with longtail potential. 
+            Then I want a list in English as a csv table, 
+            with the following columns:  cluster, keyword, search intent, language. Here are the keywords: ${_prompt}`,
             temperature: 0,
             max_tokens: 1000
         }
@@ -47,13 +53,18 @@ class Conversation {
         const results = _rawResponse.data.choices
         return results
     }
+
+    async ask(prompt) {
+        const rawResponse = await this._getTextToText(prompt)
+        const results = cleanTextToTextResponse(rawResponse)    
+        return results    
+    }
 }
 
 const test = async () => {
-    const rawResponse = await getTextToText("give me a list of keywords related to both tshirts and pizza")
-    const results = cleanTextToTextResponse(rawResponse)
-    console.log(results)
-    return results
+    const convo = new Conversation()
+    const response = Conversation.ask("personalized album poster, album poster, downloadable artist poster, musical artist poster, band poster")
+    console.log(call)
 }
 
 test()
